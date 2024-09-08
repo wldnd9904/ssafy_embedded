ff6a50ae8a4e1a8bc7599a4c34130d47a2d530a7
최지웅 <wldnd9904@uos.ac.kr>
2024-09-09 00:00:00 +0900

    240909_linux_shell_1

---
+++ 240909_linux_shell_1/과제2/과제2.bash
@@ -0,0 +1,20 @@
+# 1. home directory 이동 후, 전체 디렉토리 확인
+cd ~
+ls -al
+# 2. 현재 디렉토리가 어디인지 확인한다.
+pwd
+# 3. root directory 이동 후, 전체 디렉토리 확인
+cd /
+ls -al
+# 4. 화면을 모두 지운다.
+clear
+# 5. /usr/bin 으로 이동한다.
+cd /usr/bin
+# 6. 화면을 모두 지운다.
+clear
+# 7. 이전 디렉토리로 이동한다.
+cd -
+# 8. 현재 디렉토리가 어디인지 출력한다.
+pwd
+# 9. 터미널을 종료한다. (exit 명령어)
+exit
